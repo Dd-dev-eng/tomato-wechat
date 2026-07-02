ca37914c72fa18cf67ecd2e581d1a2dc339f7bd2
Dd-dev-eng <2213819679@qq.com>
2026-07-03 00:50:35 +0800

    debug: 最简 10 行 app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,9 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 静态文件
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => { res.send('OK'); });
 
-// 首页
-app.get('/', (req, res) => {
-  res.send('OK - 主动番茄');
-});
-
-// 启动
 app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server running on', PORT);
+  console.log('running on', PORT);
 });
